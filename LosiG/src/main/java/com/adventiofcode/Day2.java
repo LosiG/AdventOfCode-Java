1d1f03cb3556cc77a7c06487e63c8085d9903c58
LosiG <losigabriele03@gmail.com>
2024-12-02 21:24:45 +0100

    almost finieshed

--- LosiG/src/main/java/com/adventiofcode/Day2.java
+++ LosiG/src/main/java/com/adventiofcode/Day2.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -27,7 +28,7 @@ public class Day2 {
             e.printStackTrace();
         }
 
-        exercise1(data);
+        // exercise1(data);
         exercise2(data);
 
     }
@@ -70,5 +71,62 @@ public class Day2 {
     }
 
     private static void exercise2(ArrayList<String> data) {
+        ArrayList<int[]> reports = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++) {
+            int[] reportValues = Arrays.stream(data.get(i).split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            reports.add(reportValues);
+        }
+
+        int correctReports = 0;
+
+        for (int i = 0; i < reports.size(); i++) {
+            int[] report = reports.get(i);
+
+            int errorThreshold = 0;
+            Boolean isReportDescising = report[0] > report[report.length - 1];
+            Boolean isReportCorrect = true;
+
+            for (int j = 0; j < report.length - 1; j++) {
+                int currentDelta = 0;
+                final Integer currentIndex = j;
+
+                if (Boolean.TRUE.equals(isReportDescising)) {
+                    currentDelta = report[j] - report[j + 1];
+                } else {
+                    currentDelta = report[j + 1] - report[j];
+                }
+
+                isReportCorrect = currentDelta >= 1 && currentDelta <= 3;
+
+                if (!Boolean.TRUE.equals(isReportCorrect))
+                    if (errorThreshold == 0) {
+                        int[] cleanedReport = Arrays.stream(report).filter(value -> value != currentIndex).toArray();
+                        int cleanedDelta = 0;
+                        Boolean isCleanedReportCorrect = true;
+
+                        for (int k = 0; k < cleanedReport.length; k++) {
+                            if (Boolean.TRUE.equals(isReportDescising)) {
+                                cleanedDelta = cleanedReport[j] - cleanedReport[j + 1];
+                            } else {
+                                cleanedDelta = cleanedReport[j + 1] - cleanedReport[j];
+                            }
+                            isCleanedReportCorrect = cleanedDelta >= 1 && cleanedDelta <= 3;
+
+                        }
+                        errorThreshold++;
+                        if (!Boolean.TRUE.equals(isCleanedReportCorrect))
+                            break;
+
+                    } else {
+                        break;
+
+                    }
+            }
+            if (Boolean.TRUE.equals(isReportCorrect))
+                correctReports++;
+        }
+        System.out.println(correctReports);
     }
 }
